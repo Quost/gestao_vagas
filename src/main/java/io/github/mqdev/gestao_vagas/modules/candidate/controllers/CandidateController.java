27a41bcce3f27c6a1651007a274aaba67b2752e5
Matheus Quost <matheusquost@gmail.com>
2024-05-12 20:07:02 -0300

    feat: Add UserAlreadyExistsException class and handle duplicate user creation

--- src/main/java/io/github/mqdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/io/github/mqdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -3,9 +3,12 @@ package io.github.mqdev.gestao_vagas.modules.candidate.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.mqdev.gestao_vagas.exceptions.UserAlreadyExistsException;
 import io.github.mqdev.gestao_vagas.modules.candidate.CandidateEntity;
+import io.github.mqdev.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,10 +17,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/candidate")
 public class CandidateController {
     
+    @Autowired
+    private CandidateRepository candidateRepository;
     
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidate) {
-        System.out.println("Candidate created!");
-        System.out.println(candidate.toString());
+    public @Valid CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+            .ifPresent(candidate -> {
+                throw new UserAlreadyExistsException("Username or email already in use");
+            });
+        return this.candidateRepository.save(candidateEntity);
     }
 }
