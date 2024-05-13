63ad3d10b611ec8ee75d278f22285eccfea9b751
Matheus Quost <matheusquost@gmail.com>
2024-05-12 21:57:14 -0300

    feat: Update security configuration to restrict public access to jobs posts

--- src/main/java/io/github/mqdev/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/io/github/mqdev/gestao_vagas/security/SecurityConfig.java
@@ -10,7 +10,12 @@ public class SecurityConfig {
     
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/candidate/").permitAll()
+            .requestMatchers("/company/").permitAll()
+            .anyRequest().authenticated();
+        });
         return http.build();
     }
 }
