1c840c9a385072bafc0209be18d87ff16c01be6d
Matheus Quost <matheusquost@gmail.com>
2024-05-13 19:20:24 -0300

    feat: Add password encryption for candidate registration

--- src/main/java/io/github/mqdev/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/io/github/mqdev/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package io.github.mqdev.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import io.github.mqdev.gestao_vagas.exceptions.EntityAlreadyExistsException;
@@ -9,15 +10,22 @@ import io.github.mqdev.gestao_vagas.modules.candidate.CandidateRepository;
 
 @Service
 public class CreateCandidateUseCase {
-    
+
     @Autowired
     private CandidateRepository candidateRepository;
-    
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-            .ifPresent(candidate -> {
-                throw new EntityAlreadyExistsException("Username or email already in use");
-            });
+                .ifPresent(candidate -> {
+                    throw new EntityAlreadyExistsException("Username or email already in use");
+                });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+
         return this.candidateRepository.save(candidateEntity);
     }
 }
