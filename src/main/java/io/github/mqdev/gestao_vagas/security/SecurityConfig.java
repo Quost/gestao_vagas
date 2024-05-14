41b7cec0e03ee9317466dc25b25eac0b4aceb5cd
Matheus Quost <matheusquost@gmail.com>
2024-05-13 21:11:35 -0300

    refactor: Rename SecurityFilter to SecurityCompanyFilter

--- src/main/java/io/github/mqdev/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/io/github/mqdev/gestao_vagas/security/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class SecurityConfig {
 
     @Autowired
-    private SecurityFilter securityFilter;
+    private SecurityCompanyFilter securityFilter;
 
     @Autowired
     private SecurityCandidateFilter securityCandidateFilter;
