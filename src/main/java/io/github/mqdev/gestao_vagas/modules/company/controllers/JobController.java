d3f1202fc6f144b4dc1855e2c608e54b87fbac3f
Matheus Quost <matheusquost@gmail.com>
2024-05-21 20:35:32 -0300

    feat: Add Swagger annotations and security requirements to job creation endpoint

--- src/main/java/io/github/mqdev/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/io/github/mqdev/gestao_vagas/modules/company/controllers/JobController.java
@@ -13,6 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import io.github.mqdev.gestao_vagas.modules.company.dto.CreateJobDTO;
 import io.github.mqdev.gestao_vagas.modules.company.entities.JobEntity;
 import io.github.mqdev.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -25,6 +32,16 @@ public class JobController {
 
     @PostMapping("/")
     @PreAuthorize("hasRole('COMPANY')")
+    @Tag(name = "Vagas", description = "Endpoints para vagas")
+    @Operation(summary = "Cadastrar vagas disponíveis", description = "Cadastrar vagas disponíveis de uma empresa")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", content = {
+            @Content(
+                schema = @Schema(implementation = JobEntity.class)
+            )
+        }, description = "Vagas encontradas"),
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO CreateJobDTO, HttpServletRequest request) {
         var companyId = request.getAttribute("company_id");
 
