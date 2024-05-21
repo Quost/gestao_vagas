c59c03a4d59f680775a59b6ec20c673605e83350
Matheus Quost <matheusquost@gmail.com>
2024-05-21 20:35:41 -0300

    refactor: Update CreateJobDTO with Swagger annotations and examples

--- src/main/java/io/github/mqdev/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/io/github/mqdev/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,14 +1,22 @@
 package io.github.mqdev.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
+    @Schema(description = "Título da vaga", example = "Desenvolvedor Java", required = true)
     private String title;
+    @Schema(description = "Descrição da vaga", example = "Desenvolvimento de aplicações Java")
     private String description;
+    @Schema(description = "Benefícios da vaga", example = "Vale transporte, vale refeição")
     private String benefits;
+    @Schema(description = "Requisitos da vaga", example = "Conhecimento em Java, Spring Boot")
     private String requirements;
+    @Schema(description = "Salário da vaga", example = "R$ 5.000,00")
     private String salary;
+    @Schema(description = "Localização da vaga", example = "São Paulo")
     private String location;
+    @Schema(description = "Nível da vaga", example = "Júnior")
     private String level;
 }
