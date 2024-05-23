6158967c8f599ca77a4d243af6c7161c3a3a21cc
Matheus Quost <matheusquost@gmail.com>
2024-05-23 11:51:14 -0300

    refactor: Update ProfileCandidateUseCase to throw CandidateNotFoundException

--- src/main/java/io/github/mqdev/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
+++ src/main/java/io/github/mqdev/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -5,9 +5,9 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.github.mqdev.gestao_vagas.exceptions.CandidateNotFoundException;
 import io.github.mqdev.gestao_vagas.modules.candidate.CandidateRepository;
 import io.github.mqdev.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
-import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProfileCandidateUseCase {
@@ -18,7 +18,7 @@ public class ProfileCandidateUseCase {
     public ProfileCandidateResponseDTO execute(UUID candidateId) {
         var candidate = this.candidateRepository.findById(candidateId)
                 .orElseThrow(() -> {
-                    throw new EntityNotFoundException("Candidate not found");
+                    throw new CandidateNotFoundException();
                 });
 
         var candidateDTO = ProfileCandidateResponseDTO.builder()
